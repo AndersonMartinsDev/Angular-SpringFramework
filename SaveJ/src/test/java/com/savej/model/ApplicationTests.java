e89234762a9ee110f317d92180ddd8c576a1d196
LeadLamps <foxvc22@gmail.com>
2019-04-30 21:28:10 -0300

    PROGRESSO

---
+++ SaveJ/src/test/java/com/savej/model/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.savej.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTests.class, args);
+	}
+}
