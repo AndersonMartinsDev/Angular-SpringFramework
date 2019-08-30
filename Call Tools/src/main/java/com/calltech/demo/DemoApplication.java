cbafcd9b7f1645657003ba66d5a46e923c4d429b
LeadLamps <foxvc22@gmail.com>
2019-08-30 14:02:44 -0300

    ...

---
+++ Call Tools/src/main/java/com/calltech/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.calltech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.calltech.controller"})
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
