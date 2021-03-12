9c4ee6c0d3a7981a98960027c9f4a1dde17ebf8c
Anderson Santos <anderson.santos@senior.com.br>
2021-03-12 17:36:36 -0300

    resolvendo treta do Cors

--- src/main/java/com/calltech/security/config/OAuthConfig.java
+++ src/main/java/com/calltech/security/config/OAuthConfig.java
@@ -8,13 +8,19 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.Arrays;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -40,6 +46,21 @@ public class OAuthConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(HttpMethod.OPTIONS,"/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST","OPTIONS"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+
+        // You can customize the following part based on your project, it's only a sample
+        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
+                .authenticated().and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
+
     }
 
     @Bean
